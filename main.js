799359127a88015c5fb1f4d590e0a3d0ab234366
Saniagade <gadesania@gmail.com>
2025-07-26 20:07:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been received.");
+  form.reset();
+});
